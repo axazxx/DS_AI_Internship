8c2460e15c3857257f91091d3377c819252a8f7f
Anas AR <anasabdulraheem67@gmail.com>
2026-02-04 12:44:18 +0530

    day3

--- src/day3_collections.py
+++ src/day3_collections.py
@@ -24,5 +24,5 @@ print("last 3 hours",temperatures[:-3])
 
 screen_res=(1920,1080)
 print("Current Resolution:1920x1080")
-screen_res[0]=1280
-print("tuples cannot be changed")
+#screen_res[0]=1280
+print("tuples cannot be modified!")
