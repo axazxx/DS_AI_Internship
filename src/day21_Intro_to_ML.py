8884d34583b985f3088048e5631bad4965ef2b40
Anas AR <anasabdulraheem67@gmail.com>
2026-02-25 12:44:33 +0530

    day21

---
+++ src/day21_Intro_to_ML.py
@@ -0,0 +1,29 @@
+# TASK 1
+#
+# Machine Learning vs Traditional coding:
+#    Machine learning is a subset of Artificial Intelligence. It is used to understand
+#    patterns and learn from a given set of examples. In Traditional coding the coder gives 
+#    instructions and rules for the code to execute whereas ML learns from the given input and
+#    output and learns their patterns.
+#    For example, if you want to detect spam emails, you might write rules like: “If the email
+#    contains ‘free money,’ mark it as spam.” The computer simply follows the instructions you provide.
+#    Whereas for ML, you give it thousands of emails labeled “spam” or “not spam,” and the algorithm
+#    figures out which patterns best separate the two categories. The “rules” are not hand-written—they 
+#    are learned from data.
+#
+#
+# TASK 2
+#
+#    Machine Learning consists of Three types of learning:
+#        1 Supervised learning= In supervised learning the computer is given inputs aswell as their outputs
+#          to learn and use them as training data.
+#          ex: Spam Detection where email classification systems are trained on labeled data 
+#          (e.g., "spam" vs. "not spam") to filter future emails.
+#        2 Unsupervised learning= In Unsupervised No labels are given to the learning algorithm, leaving it on its own to find
+#          the hidden patterns.
+#          ex: Customer Segmentation where analyzing purchase behavior data to cluster customers into groups based on 
+#          similarities such as high purchase and low purchase.
+#        3 Reinforcement learning= the computer reaches its goals through trial and error.
+#          ex: Gaming AI such as chess game which rewards on correct steps and penalizes on wrong steps.
+#        
+#
